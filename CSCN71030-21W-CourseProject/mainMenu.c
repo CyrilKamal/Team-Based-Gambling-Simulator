9572ad078273609ce16fa7f1e96442b5d3ad931b
Cyril Kamal <43249970+CyrilKamal@users.noreply.github.com>
2021-04-17 06:59:52 -0400

    Initialize SlotsV2

---
+++ CSCN71030-21W-CourseProject/mainMenu.c
@@ -0,0 +1,31 @@
+#include "mainMenu.h"
+
+void mainMenuChoices()
+{
+	printf("Please type in the number label of the task you would like to do\n");
+	printf("1 -> Blackjack\n");
+	printf("2 -> Roulette\n");
+	printf("3 -> Slots\n");
+	printf("4 -> Current Balance\n");
+	printf("5 -> Exit Game\n");
+}
+
+void toFile(int balance, int start)
+{
+	// save starting balance and ending balance to file
+	FILE* fp;
+
+	fp = fopen("GameHistory.txt", "a");
+
+	if (fp == NULL)
+	{
+		printf("Error opening file.\n\n");
+		exit(EXIT_FAILURE);
+	}
+
+	fprintf(fp, "Starting Balance: $%d\n", start);
+	fprintf(fp, "Ending Balance: $%d\n\n", balance);
+
+	fclose(fp);
+	return;
+}
